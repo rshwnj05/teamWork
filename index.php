51f0c57b1f62ebc4d190b993d7a0caca1576986c
rshwnj05 <rshwnj05@yahoo.com>
2016-10-12 15:23:40 -0600

    added my index page

--- index.php
+++ index.php
@@ -4,11 +4,36 @@
     <meta charset="utf-8">
     <title></title>
     <?php require_once("lib/headutils.php"); ?>
+    <link rel="stylesheet" href="lib/css/index.css"/>
 
   </head>
   <body>
-
     <?php require_once("lib/partials/navbar.php"); ?>
 
+<div class="container">
+    <div class="jumbotron">
+      <h1>Hello this is my index page<h1>
+        <p>paragraph text
+    </div>
+
+<div class="container">
+  <div class="col-md-4">
+  <h2>Heading<h2>
+  <p>paragraph text
+</div>
+
+<div class="container">
+  <div class="col-md-4">
+  <h2>Heading<h2>
+  <p>paragraph text
+</div>
+
+<div class="container">
+  <div class="col-md-4">
+  <h2>Heading<h2>
+  <p>paragraph text
+</div>
+
+    </div>
   </body>
-</html> 
+</html>
