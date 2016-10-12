64abae3b0a5dd436d931ce5373f474dd7bafc110
chenoajai <winsteadchenoa@gmail.com>
2016-10-12 15:25:35 -0600

    added jumbotron and text to about.php

--- about.php
+++ about.php
@@ -2,12 +2,41 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+
     <?php require_once("lib/headutils.php");?>
+    <link rel="stylesheet" href="lib/css/about.css">
+
+
+
 
   </head>
   <body>
     <?php require_once("lib/partials/navbar.php");?>
+
+    <div class="jumbotron" id="jumbotron1">
+      <h1 id="headText">This is my about page</h1>
+      <p id="headP"> Welcome to my about page!</p>
+    </div>
+
+
+
+      <div class="container">
+        <div class="col-md-4 block">
+          <h1> ABOUT</h1>
+          <p>i dont know what will go here yet </p>
+        </div>
+
+        <div class="col-md-4 block">
+          <h2> ABOUT2</h2>
+          <p> Something else will go here i dont know yet </p>
+        </div>
+
+        <div class="col-md-4 block">
+          <h3> ABOUT3</h3>
+          <p> And again something will go here.</p>
+        </div>
+
+      </div>
 
   </body>
 </html>
--- lib/headutils.php
+++ lib/headutils.php
@@ -1,2 +1,4 @@
 <link rel="stylesheet" href="lib/css/bootstrap.css">
+<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+
 <link rel="stylesheet" href="lib/css/style.css">
